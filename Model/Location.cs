c4e8e86043781ac4b87ea25bb254eb726749142c
gozden2001 <gozdenovicboris@gmail.com>
2024-03-10 13:25:55 +0100

    Klasa za lokaciju

---
+++ Model/Location.cs
@@ -0,0 +1,36 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Location : ISerializable
+    {
+        int locationId;
+        string City { get; set; }
+        string Country { get; set; }
+
+        public Location() { }
+        public Location(string city, string country)
+        {
+            City = city;
+            Country = country;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            locationId = Convert.ToInt32(values[0]);
+            City = values[1];
+            Country = values[2];
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { locationId.ToString(), City, Country };
+            return csvValues;
+        }
+    }
+}
