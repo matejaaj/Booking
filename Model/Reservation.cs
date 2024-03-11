43c9fb2c605fa27aa855de0bf36ae577620ca7f7
gozden2001 <gozdenovicboris@gmail.com>
2024-03-11 19:56:42 +0100

    Restart projekta

---
+++ Model/Reservation.cs
@@ -0,0 +1,68 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Reservation : ISerializable
+    {
+        public int ReservationId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Days { get; set; }
+        public int GuestNumber { get; set; }
+        public Accommodation Accommodation { get; set; }
+        public User Guest { get; set; }
+        public Reservation()
+        {
+        }
+        public Reservation(DateTime startDate, DateTime endDate, int days, int guestNumber, Accommodation accommodation, User guest)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Days = days;
+            GuestNumber = guestNumber;
+            Accommodation = accommodation;
+            Guest = guest;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { ReservationId.ToString(), StartDate.ToString(), EndDate.ToString(), Days.ToString(), GuestNumber.ToString(), Accommodation.AccommodationId.ToString(), Guest.Id.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            ReservationId = Convert.ToInt32(values[0]);
+            DateTime startDate;
+            if (DateTime.TryParseExact(values[1], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out startDate))
+            {
+                StartDate = startDate;
+            }
+            else
+            {
+                // Handle parsing failure
+                Console.WriteLine("Failed to parse start date from CSV.");
+            }
+            DateTime endDate;
+            if (DateTime.TryParseExact(values[2], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out endDate))
+            {
+                EndDate = endDate;
+            }
+            else
+            {
+                // Handle parsing failure
+                Console.WriteLine("Failed to parse start date from CSV.");
+            }
+            Days = Convert.ToInt32(values[3]);
+            GuestNumber = Convert.ToInt32(values[4]);
+            Accommodation = new Accommodation() { AccommodationId = Convert.ToInt32(values[5]) };
+            Guest = new User() { Id = Convert.ToInt32(values[6]) };
+        }
+
+    }
+}
---
+++ Repository/ReservationRepository.cs
@@ -0,0 +1,73 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class ReservationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/reservation.csv";
+        private readonly Serializer<Reservation> _serializer;
+        private List<Reservation> _reservation;
+
+        public ReservationRepository()
+        {
+            _serializer = new Serializer<Reservation>();
+            _reservation = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Reservation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Reservation Save(Reservation reservation)
+        {
+            reservation.ReservationId = NextId();
+            _reservation = _serializer.FromCSV(FilePath);
+            _reservation.Add(reservation);
+            _serializer.ToCSV(FilePath, _reservation);
+            return reservation;
+        }
+
+        public int NextId()
+        {
+            _reservation = _serializer.FromCSV(FilePath);
+            if (_reservation.Count < 1)
+            {
+                return 1;
+            }
+            return _reservation.Max(a => a.ReservationId) + 1;
+        }
+
+        public void Delete(Reservation reservation)
+        {
+            _reservation = _serializer.FromCSV(FilePath);
+            Reservation founded = _reservation.Find(a => a.ReservationId == reservation.ReservationId);
+            _reservation.Remove(founded);
+            _serializer.ToCSV(FilePath, _reservation);
+        }
+
+        public Reservation Update(Reservation reservation)
+        {
+            _reservation = _serializer.FromCSV(FilePath);
+            Reservation current = _reservation.Find(a => a.ReservationId == reservation.ReservationId);
+            int index = _reservation.IndexOf(current);
+            _reservation.Remove(current);
+            _reservation.Insert(index, reservation);
+            _serializer.ToCSV(FilePath, _reservation);
+            return reservation;
+        }
+
+        public List<Reservation> GetByUser(User user)
+        {
+            _reservation = _serializer.FromCSV(FilePath);
+            return _reservation.FindAll(a => a.Guest.Id == user.Id);
+        }
+    }
+
+}
