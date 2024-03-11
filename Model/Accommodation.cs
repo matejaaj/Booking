a0fe3939476d2875330206e0b035418e1fd9401a
gozden2001 <gozdenovicboris@gmail.com>
2024-03-11 22:08:18 +0100

    dovrsena registracija novih smjestaja

--- Model/Accommodation.cs
+++ Model/Accommodation.cs
@@ -13,11 +13,11 @@ namespace BookingApp.Model
     {
         public int AccommodationId { get; set; }   
         public string Name { get; set; }
-        public Location Location {  get; set; }
+        public int LocationId {  get; set; }
         public Type Type { get; set; }
-        public int maxGuests { get; set; }
-        public int minReservations { get; set; }
-        public int cancelThershold { get; set; }
+        public int MaxGuests { get; set; }
+        public int MinReservations { get; set; }
+        public int CancelThershold { get; set; }
         public List<string> imageSource;
         public User Owner;
 
@@ -26,12 +26,16 @@ namespace BookingApp.Model
             imageSource = new List<string>();
         }
 
-        public Accommodation(string name, int maxGuests, int minReservations, int cancelThershold)
+        public Accommodation(string name, int locationId, string type, int maxGuests, int minReservations, int cancelThershold, User owner)
         {
             Name = name;
-            this.maxGuests = maxGuests;
-            this.minReservations = minReservations;
-            this.cancelThershold = cancelThershold;
+            LocationId = locationId;
+            Enum.TryParse(type, out Type typeEnum);
+            Type = typeEnum;
+            MinReservations = minReservations;
+            MaxGuests = maxGuests;
+            CancelThershold = cancelThershold;
+            Owner = owner;
             imageSource = new List<string>();
         }
 
@@ -39,26 +43,18 @@ namespace BookingApp.Model
         {
             AccommodationId = Convert.ToInt32(values[0]);
             Name = values[1];
-            Location = new Location() { locationId = Convert.ToInt32(values[2]) };
-            Type accommodationType;
-            if (Enum.TryParse(values[3], out accommodationType))
-            {
-                Type = accommodationType;
-            }
-            else
-            {
-                Console.WriteLine($"Error converting '{values[3]}' to Type enum. Setting default value.");
-                Type = Type.APARTMENT;
-            }
-            maxGuests = Convert.ToInt32(values[4]);
-            minReservations = Convert.ToInt32(values[5]);
-            cancelThershold = Convert.ToInt32(values[6]);
+            LocationId = Convert.ToInt32(values[2]);
+            Enum.TryParse(values[3], out Type typeEnum);
+            Type = typeEnum;
+            MaxGuests = Convert.ToInt32(values[4]);
+            MinReservations = Convert.ToInt32(values[5]);
+            CancelThershold = Convert.ToInt32(values[6]);
             Owner = new User() { Id = Convert.ToInt32(values[7]) };
         }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { AccommodationId.ToString(), Name, Location.locationId.ToString(), Type.ToString(), maxGuests.ToString(), minReservations.ToString(), cancelThershold.ToString(), Owner.Id.ToString() };
+            string[] csvValues = { AccommodationId.ToString(), Name, LocationId.ToString(), Type.ToString(), MaxGuests.ToString(), MinReservations.ToString(), CancelThershold.ToString(), Owner.Id.ToString() };
             return csvValues;
         }
     }
--- Model/Location.cs
+++ Model/Location.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BookingApp.Model
 {
-     class Location : ISerializable
+     public class Location : ISerializable
     {
         public int locationId;
         public string City { get; set; }
@@ -31,6 +31,11 @@ namespace BookingApp.Model
         {
             string[] csvValues = { locationId.ToString(), City, Country };
             return csvValues;
+        }
+
+        public override string ToString()
+        {
+            return $"{City}, {Country}";
         }
     }
 }
