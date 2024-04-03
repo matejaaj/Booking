4ed51892a58201aa55307e678eaecd2adab67fd3
matejaaj <mateja.odzaci@gmail.com>
2024-04-03 13:34:11 +0200

    injector class

---
+++ Application/Injector.cs
@@ -0,0 +1,40 @@
+﻿using BookingApp.Domain.RepositoryInterfaces;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Application
+{
+    public class Injector
+    {
+        private static Dictionary<Type, object> _implementations = new Dictionary<Type, object>
+    {
+        { typeof(IUserRepository), new UserRepository() },
+        { typeof(ITourRepository), new TourRepository() },
+        { typeof(IVehicleRepository), new VehicleRepository() },
+        { typeof(ITourInstanceRepository), new TourInstanceRepository() },
+        { typeof(ITourReservationRepository), new TourReservationRepository() },
+        { typeof(ITourGuestRepository), new TourGuestRepository() },
+        { typeof(IDriveReservationRepository), new DriveReservationRepository() },
+        { typeof(ILanguageRepository), new LanguageRepository() },
+        { typeof(IImageRepository), new ImageRepository() },
+        { typeof(ICheckpointRepository), new CheckpointRepository() },
+        { typeof(IDetailedLocationRepository), new DetailedLocationRepository() }
+    };
+
+        public static T CreateInstance<T>()
+        {
+            Type type = typeof(T);
+
+            if (_implementations.ContainsKey(type))
+            {
+                return (T)_implementations[type];
+            }
+
+            throw new ArgumentException($"No implementation found for type {type}");
+        }
+    }
+}
