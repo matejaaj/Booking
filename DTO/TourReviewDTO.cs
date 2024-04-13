5c3adfcb4c35c3e819cd80e3ce53ed5ce710a2d8
Bogdan Djukic <bogdandjukic25@gmail.com>
2024-04-13 14:17:35 +0200

    reviewsDTo and window for selecting instances

---
+++ DTO/TourReviewDTO.cs
@@ -0,0 +1,179 @@
+﻿using BookingApp.Application.UseCases;
+using BookingApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.DTO
+{
+    internal class TourReviewDTO : INotifyPropertyChanged
+    {
+
+        private readonly TourGuestService _tourGuestService;
+        private readonly CheckpointService _checkpointService;
+
+        private string _tourGuest;
+        private int _id;
+        private int _tourId;
+        private int _touristId;
+        private int _tourGuestId;
+        private int _rating;
+        private string _comment;
+        private bool _isValid;
+        private string _checkpoint;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public string Checkpoint
+        {
+            get { return _checkpoint; }
+            set
+            {
+                if( _checkpoint != value)
+                {
+                    _checkpoint = value;
+                    OnPropertyChanged(nameof(Checkpoint));
+                }
+            }
+        }
+
+        public int TourId
+        {
+            get { return _tourId; }
+            set
+            {
+                if (_tourId != value)
+                {
+                    _tourId = value;
+                    OnPropertyChanged(nameof(TourId));
+                }
+            }
+        }
+
+        public int TouristId
+        {
+            get { return _touristId; }
+            set
+            {
+                if (_touristId != value)
+                {
+                    _touristId = value;
+                    OnPropertyChanged(nameof(TouristId));
+                }
+            }
+        }
+
+        public int TourGuestId
+        {
+            get { return _tourGuestId; }
+            set
+            {
+                if (_tourGuestId != value)
+                {
+                    _tourGuestId = value;
+                    OnPropertyChanged(nameof(TourGuestId));
+                }
+            }
+        }
+
+        public int Rating
+        {
+            get { return _rating; }
+            set
+            {
+                if (_rating != value)
+                {
+                    _rating = value;
+                    OnPropertyChanged(nameof(Rating));
+                }
+            }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                if (_comment != value)
+                {
+                    _comment = value;
+                    OnPropertyChanged(nameof(Comment));
+                }
+            }
+        }
+
+        public bool IsValid
+        {
+            get { return _isValid; }
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+        public string TourGuest
+        {
+            get { return _tourGuest; }
+            set
+            {
+                if(_tourGuest != value)
+                {
+                    _tourGuest = value;
+                    OnPropertyChanged(nameof(_tourGuest));
+                }
+            }
+        }
+
+
+
+        public TourReviewDTO(TourReview review)
+        {
+            Id = review.Id;
+            TourId = review.TourId;
+            TouristId = review.TouristId;
+            TourGuestId = review.TourGuestId;
+            Rating = review.Rating;
+            Comment = review.Comment;
+            IsValid = review.IsValid;
+
+            _tourGuestService = new TourGuestService();
+            _checkpointService = new CheckpointService();
+
+            TourGuest guest = _tourGuestService.GetById(TourGuestId);
+            TourGuest = guest.Name;
+
+            Checkpoint = _checkpointService.GetById(guest.CheckpointId).Name;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public TourReview TourReview(TourReviewDTO reviewDTO)
+        {
+            return new TourReview(Id, TouristId, TourGuestId, Rating, Comment, IsValid);
+        }
+    }
+}
---
+++ WPF/View/Guide/ShowReviews.xaml.cs
@@ -0,0 +1,34 @@
+﻿using BookingApp.DTO;
+using BookingApp.WPF.ViewModel.Guide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.WPF.View.Guide
+{
+    public partial class ShowReviews : Window
+    {
+        private readonly ShowReviewsViewModel _viewModel;
+        public ShowReviews(TourDTO tourDTO)
+        {
+            InitializeComponent();
+            _viewModel = new ShowReviewsViewModel(tourDTO);
+            DataContext = _viewModel;
+        }
+
+        private void btnShowReviews_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.ShowReviews();
+        }
+    }
+}
