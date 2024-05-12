9288d2067d35ca1ae848dc263ce597af0e9d2868
matejaaj <mateja.odzaci@gmail.com>
2024-05-12 20:56:43 +0200

    driver found notification

---
+++ Application/UseCases/NotificationService.cs
@@ -0,0 +1,40 @@
+﻿using BookingApp.Domain.Model;
+using BookingApp.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Application.UseCases
+{
+    public class NotificationService
+    {
+        private readonly INotificationRepository _notificationRepository;
+
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+        }
+
+        public List<Notification> GetAllNotifications()
+        {
+            return _notificationRepository.GetAll();
+        }
+
+        public void AddOrUpdateNotification(Notification notification)
+        {
+            _notificationRepository.Save(notification);
+        }
+
+        public void RemoveNotification(int id)
+        {
+            _notificationRepository.Delete(id);
+        }
+
+        public List<Notification> GetNotificationsForUser(int userId)
+        {
+            return _notificationRepository.GetAllForUserId(userId);
+        }
+    }
+}
