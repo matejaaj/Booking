77a8065e3bf5f8ff1d0fa2c192db66e7ee6b17a0
gozden2001 <gozdenovicboris@gmail.com>
2024-05-31 23:30:45 +0200

    Added Forum, leaving a comment and reporting comments

---
+++ WPF/View/Owner/ViewForumPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BookingApp.WPF.ViewModel.Owner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.WPF.View.Owner
+{
+    /// <summary>
+    /// Interaction logic for ViewForumPage.xaml
+    /// </summary>
+    public partial class ViewForumPage : Page
+    {
+        private ViewForumViewModel viewModel;
+        public ViewForumPage(Domain.Model.Owner _loggedInOwner, DTO.ForumDisplayOwnerDTO forum)
+        {
+            InitializeComponent();
+            viewModel = new ViewForumViewModel(_loggedInOwner, forum, this);
+            DataContext = viewModel;
+        }
+    }
+}
